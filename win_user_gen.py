df72c50606419dc82c5d55194a06059a69e97674
Juan Ortega <jcortega93@gmail.com>
2018-04-16 02:19:25 -0700

    Update win_user_gen.py

--- win_user_gen.py
+++ win_user_gen.py
@@ -1,28 +1,42 @@
+#Author: Juan Ortega
+#Date: 04/16/2018
 
-import subprocess
+from subprocess import check_output
+import logging
 from faker import Faker
+fake = Faker()
 
 def main():
-  cmd = "net user /add{}".format(username)
-  for _ in range(10000):
-    try:
-      username = fake.user_name
-      run_win_cmd(cmd)
-    except:
-       print(username + ": already exists"
+    while True:
+        try:
+            user_amount = int(input("How many fake users do you want to create? \n"))
+            break
+        except:
+            print("Error: Input must be an integer \n")
+
+    while True:
+
+        for _ in range(user_amount):
+            try:
+                username = fake.user_name()
+                password = fake.password(length=8, special_chars=True, digits=True, upper_case=False, lower_case=True)
+                cmd = "net user /add {} \"{}\"".format(str(username),password)
+                run_win_cmd(cmd)
+                logger.info(str(username) + "," + str(password))
+            except:
+                pass
+        break
+
 
 def run_win_cmd(cmd):
-  result = []
-  process = subprocess.Popen(cmd,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-  for line in process.stdout:
-      result.append(line)
-  errcode = process.returncode
-  for line in result:
-      print(line)
-  if errcode is not None:
-      raise Exception('cmd %s failed, see above for details', cmd)
+    check_output(cmd, shell=True).decode()
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    hdlr = logging.FileHandler('user_list.log')
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    hdlr.setLevel(logging.INFO)
+    logger.addHandler(hdlr)
+    main()
