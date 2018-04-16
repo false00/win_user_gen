c844d14b14fb7ea1ceaeaccabafe4f75532a0cf5
Juan Ortega <jcortega93@gmail.com>
2018-04-16 02:22:04 -0700

    Update win_user_gen.py

--- win_user_gen.py
+++ win_user_gen.py
@@ -1,5 +1,21 @@
-#Author: Juan Ortega
-#Date: 04/16/2018
+#!/usr/bin/python3
+#
+# NAME - win_user_gen
+# Version 1.0
+#
+# SYNOPSIS
+# python3 ./win_user_gen.py
+#
+# DESCRIPTION
+# Creates random user accounts on windows
+#
+# AUTHOR
+# Juan Ortega
+#
+# HISTORY:
+#
+# Date(YYYY/MM/DD):     Version:        Modified By:    Description of Change:
+# 2018/04/16            1.0           Juan Ortega     First version of script
 
 from subprocess import check_output
 import logging
